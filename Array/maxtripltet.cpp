0b73cf776216c06b21319e87af8684491904e700
sujal-malviya <sujal.malviya2022@gmail.com>
2025-07-10 22:34:08 +0530

    maximum triple product

---
+++ Array/maxtripltet.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int maxTripleProduct(vector<int>& arr) {
+    int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
+    int min1 = INT_MAX, min2 = INT_MAX;
+
+    for (int num : arr) {
+        // update max values
+        if (num > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = num;
+        } else if (num > max2) {
+            max3 = max2;
+            max2 = num;
+        } else if (num > max3) {
+            max3 = num;
+        }
+
+        // update min values
+        if (num < min1) {
+            min2 = min1;
+            min1 = num;
+        } else if (num < min2) {
+            min2 = num;
+        }
+    }
+
+    return max(max1 * max2 * max3, min1 * min2 * max1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << maxTripleProduct(arr) << endl;
+    return 0;
+}
