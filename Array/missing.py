e0631b9d906fdfa4836006700060330d02d3ccf0
sujal-malviya <sujal.malviya2022@gmail.com>
2025-07-13 01:18:17 +0530

    Python

--- Array/missing.py
+++ Array/missing.py
@@ -1,16 +1,27 @@
 #Missing Number - Sum = n(n+1)/2
-mylist = [13,14,11]
-def missing(mylist):
-    indx = 0
-    for i in range(len(mylist)):
-        j= i+1
-        if (j == len(mylist)):
-            break
-        subt = abs(mylist[i] - mylist[j])
-        if(subt == 1):
-            continue
-        else:
-            indx = mylist[i]+1;
-    return indx
-print(missing(mylist))                
+# mylist = [13,14,11]
+# def missing(mylist):
+#     indx = 0
+#     for i in range(len(mylist)):
+#         j= i+1
+#         if (j == len(mylist)):
+#             break
+#         subt = abs(mylist[i] - mylist[j])
+#         if(subt == 1):
+#             continue
+#         else:
+#             indx = mylist[i]+1;
+#     return indx
+# print(missing(mylist))                
     
+# Day 5: Finding the Missing Number in a Consecutive List
+mylist = [11, 13, 14]                                           # Example list with a missing number
+
+def missing(mylist):
+    mylist.sort()                                               # Sort the list to ensure correct order
+    for i in range(len(mylist) - 1):
+        if mylist[i+1] - mylist[i] != 1:
+            return mylist[i] + 1
+    return -1                                                  # Return -1 if no missing number found
+
+print(missing(mylist))
--- Array/unique.py
+++ Array/unique.py
@@ -1,16 +1,23 @@
-mylist = [0,1,1,0,3]
+# mylist = [0,1,1,0,3]
 
+# def unique(mylist):
+    
+#     a = 0
+#     for i in range(len(mylist)):
+#         for j in range(len(mylist)):
+#             if mylist[i] != mylist[j]:
+#                 a = mylist[i]
+#     return a
+# print(unique(mylist))
 def unique(mylist):
-    
-    a = 0
     for i in range(len(mylist)):
+        count = 0
         for j in range(len(mylist)):
-            if mylist[i] != mylist[j]:
-                a = mylist[i]
-    return a
-print(unique(mylist))
+            if mylist[i] == mylist[j]:
+                count += 1
+        if count == 1:
+            return mylist[i]
 
-    
     
     
     
