0da492582bbf4fc89f49ecfc2a2d7b96c840c7bc
sujal-malviya <sujal.malviya2022@gmail.com>
2025-07-22 22:00:26 +0530

    python

--- Array/miss_repeat.py
+++ Array/miss_repeat.py
@@ -1,3 +1,14 @@
+arr = [3,1,3]
+def findTwoElement(arr):
+    n = len(arr)
+    sum_n = n * (n + 1) // 2
+    sum_sq_n = n * (n + 1) * (2 * n + 1) // 6
+
+    sum_arr = sum(arr)
+    sum_sq_arr = sum(x * x for x in arr)
+
+    diff = sum_arr - sum_n           
+    sq_diff = sum_sq_arr - sum_sq_n  
 
     sum_xy = sq_diff // diff        
 
