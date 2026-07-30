f6fceede3dd213315d5a2571d2068f022064ab17
sujal-malviya <sujal.malviya2022@gmail.com>
2026-07-30 13:53:26 +0530

    Pattern01

---
+++ practis2026/Patterns/pattern.java
@@ -0,0 +1,21 @@
+package practis2026.Patterns;
+
+import java.util.Scanner;
+
+public class pattern {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i = 0;i<n;i++)
+        {
+            for(int j =0;j<n;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
+
+
