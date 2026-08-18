cf71c95ee90ca012bb6f27760f99f5114dfbb65e
sujal-malviya <sujal.malviya2022@gmail.com>
2026-08-18 10:52:38 +0530

    Pallindrome Number

--- BasicMath/Basic05.java
+++ BasicMath/Basic05.java
@@ -1,5 +1,25 @@
 package BasicMath;
 
 public class Basic05 {
-    
+    static boolean isPallindrome(int number)
+    {
+        if(number==0)
+        {
+            return false;
+        }
+        int temp = number;
+        int reverse =0;
+        while(number!=0)
+        {
+            int digit= number%10;
+            reverse = reverse*10+digit;
+            number = number/10;
+
+        }
+        return temp == reverse;
+    }
+    public static void main(String[] args) {
+        boolean result = isPallindrome(121);
+        System.out.println("Pallindrome Number : "+result);
+    }
 }
